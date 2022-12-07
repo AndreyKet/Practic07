a92ea8caff0e63c75128945e4523b88252265126
Andrey <man.liher66@gmail.com>
2022-12-08 01:02:56 +0300

    ДЗ_Задача№52

---
+++ ДЗ_Задача№52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array = new int[3, 4]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4},
+};
+Console.Write("Введите номер столбца: ");
+int column = (int.Parse(Console.ReadLine()) -1);
+
+double sumAverage = 0;
+double value = 0;
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+PrintArray(array);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (j == column)
+        {
+            sumAverage += array[i, j];
+            value++;
+        }
+    }
+}
+
+sumAverage = sumAverage / value;
+Console.Write($"Average: {sumAverage}");
